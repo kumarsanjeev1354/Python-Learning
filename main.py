f9872f91e871c635294538c08de664e82ada70e8
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2023-05-13 02:39:58 +0530

    to fin the greatet of 3 numbers

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ if(a>b):
     print("a is greater")
 else:
     print("b is greater")
+    #if else expample
+    a=3
+    b=4
+    c=5
+    if(a>b):
+        print("a is greater")
+    elif(b>c):
+        print("b is greater")
+    else:
+        print("c is greater")
